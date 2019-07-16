30ffc8a6d5af14a611ae2b7690b1dad0b47ee79a
Akwe McDaniels <apmcdaniels@uchicago.edu>
2019-07-16 17:53:53 -0500

    actual submission of Assignment 1

--- hw1/Part_ii/change.py
+++ hw1/Part_ii/change.py
@@ -1,24 +1,24 @@
-def small_change(n, c_for_use, c_atm):
+def small_change(n, c_for_use, c_atm, pos):
+    if pos < 0:
+        return c_atm
+    elif n < c_for_use[pos]:
+        return small_change(n, c_for_use, c_atm, pos - 1)
+    else:
+        return small_change(n-c_for_use[pos], c_for_use, c_atm+1, pos)
+    
+def small_change2(n, c_for_use, c_atm, pos):
+    if pos < 0:
+        return c_atm
+    else:
+        fit = n//c_for_use[pos]
+        remainder = n%c_for_use[pos]
+        return small_change(remainder, c_for_use, c_atm+fit, pos-1)
+        
+if __name__ == '__main__':
     print("You're at a register, and the cashier has just handed you your change.")
     n = eval(input("How much change are you owed?"))
     coins = [.01, .05, .10, .25]
-    
-    if sum(c_atm) == n:
-        yield c_atm
-    elif sum(c_atm) > n:
-        pass
-    elif c_for_use == []:
-        pass
-    else:
-        for c in small_change(n, c_for_use[:], c_atm+[c_for_use[0]]):
-            yield c
-        for c in small_change(n, c_for_use[1:], c_atm):
-            yield c
-            
-    c_returned = [s for s in small_change(n, coins, [])]
-    
-    min(c_returned, key=len)
-    print(len)
-        
-
-small_change()
+    coins1 = small_change(n, sorted(coins), 0, len(coins)-1)
+    coins2 = small_change2(n, sorted(coins), 0, len(coins)-1)
+    print("First function returned", coins1)
+    print("Second function returned", coins2)
---
+++ practice/swapvar.py
@@ -0,0 +1,7 @@
+x = 3
+y = 4
+print("x=3 and y=4")
+print(x, y)
+print("Now, for the switch")
+x, y = y, x
+print(x, y)
